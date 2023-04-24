80079025196e3528f93cdb6ede1b93abbef421de
Erik Shelley <erik@erikshelley.com>
2023-04-24 06:56:25 -0700

    Handle text overflows

--- main.ts
+++ main.ts
@@ -35,18 +35,23 @@ var globalEval = function globalEval(src) {
 // https://d3-graph-gallery.com/graph/treemap_custom.html - beautiful example
 // https://www.d3indepth.com/hierarchies/ - good information breakdown
 var d3Template = `
-
     function wrap(text) {
         text.each(function () {
             var text = d3.select(this),
                 words = text.text().split(/\\s+/).reverse(),
                 word,
+                longline,
+                splits,
+                i,
                 line = [],
                 lineNumber = 0,
                 lineHeight = 1.1, // ems
                 x = text.attr("x"),
                 y = text.attr("y"),
-                width = text.attr("width"),
+                //width = text.attr("width"),
+                width = parseFloat(text.attr("width")),
+                height = parseFloat(text.attr("height")),
+                fontsize = parseFloat(text.attr("font-size").substring(0, text.attr("font-size").length - 2)),
                 dy = 0, //parseFloat(text.attr("dy")),
                 tspan = text.text(null)
                             .append("tspan")
@@ -56,15 +61,63 @@ var d3Template = `
             while (word = words.pop()) {
                 line.push(word);
                 tspan.text(line.join(" "));
-                if (tspan.node().getComputedTextLength() > width) {
+                if ((lineNumber + 2) * lineHeight * fontsize >= height) {
+                    tspan.text("...");
+                }
+                else if (tspan.node().getComputedTextLength() > width) {
                     line.pop();
                     tspan.text(line.join(" "));
+
+                    // if a single word is too long, break it in half
+                    if (tspan.node().getComputedTextLength() > width && tspan.text().length > 1) {
+                        longline = tspan.text();
+                        splits = 1;
+                        while (tspan.node().getComputedTextLength() > width && tspan.text().length > 1) {
+                            splits++;
+                            tspan.text(longline.substring(0, longline.length/splits) + "-");
+                        }
+                        for (i=2; i<=splits; i++) {
+                            if ((lineNumber + 2) * lineHeight * fontsize >= height) {
+                                tspan.text("...");
+                            }
+                            else {
+                                tspan = text.append("tspan")
+                                            .attr("x", x)
+                                            .attr("y", y)
+                                            .attr("dy", ++lineNumber * lineHeight + dy + "em")
+                                            .text(longline.substring((i - 1) * longline.length / splits, i * longline.length / splits) + (i < splits ? "-" : ""));
+                            }
+                        }
+                    }
+
                     line = [word];
                     tspan = text.append("tspan")
                                 .attr("x", x)
                                 .attr("y", y)
                                 .attr("dy", ++lineNumber * lineHeight + dy + "em")
                                 .text(word);
+
+                    // if last word is too long, break it in half
+                    if (words.length == 0 && tspan.node().getComputedTextLength() > width) {
+                        longline = tspan.text();
+                        splits = 1;
+                        while (tspan.node().getComputedTextLength() > width && tspan.text().length > 1) {
+                            splits++;
+                            tspan.text(longline.substring(0, longline.length/splits) + "-");
+                        }
+                        for (i=2; i<=splits; i++) {
+                            if ((lineNumber + 2) * lineHeight * fontsize >= height) {
+                                tspan.text("...");
+                            }
+                            else {
+                                tspan = text.append("tspan")
+                                            .attr("x", x)
+                                            .attr("y", y)
+                                            .attr("dy", ++lineNumber * lineHeight + dy + "em")
+                                            .text(longline.substring((i - 1) * longline.length / splits, i * longline.length / splits) + (i < splits ? "-" : ""));
+                            }
+                        }
+                    }
                 }
             }
             if (lineNumber > 0) {
@@ -74,17 +127,55 @@ var d3Template = `
         });
     };
 
+    function ellipse(text) {
+        text.each(function() {
+            var text = d3.select(this);
+            var width = parseFloat(text.attr("width"));
+            var original = text.text();
+            var tspan = text.text("").append("tspan").text(original);
+            if (tspan.node().getComputedTextLength() <= width) return;
+            tspan.text(original + "...");
+            while (tspan.node().getComputedTextLength() > width & tspan.text().length > 3) {
+                tspan.text(tspan.text().substring(0, tspan.text().length - 4) + "...");
+            }
+        });
+    }
+
+    function ellipsebyword(text) {
+        text.each(function() {
+            var text = d3.select(this);
+            var words = text.text().split(/\\s+/);
+            
+            var ellipsis = text.text("").append("tspan").text("...");
+            var width = parseFloat(text.attr("width")) - ellipsis.node().getComputedTextLength();
+            var numWords = words.length;
+            
+            var tspan = text.insert("tspan", ":first-child").text(words.join(" "));
+            
+            // Try the whole line
+            // While it's too long, and we have words left, keep removing words
+            while (tspan.node().getComputedTextLength() > width && words.length) {
+                words.pop();
+                tspan.text(words.join(" "));
+            }
+            
+            if (words.length === numWords) {
+                ellipsis.remove();
+            }
+        });
+    }
+
     // load json data into hierarchy of nodes
     // the ||!d.children adds a default value of 1 for any leaf nodes (no children) with no value
     var nodes = d3.hierarchy(data).sum(function(d) { return d.value||!d.children; });
 
     var svg = d3.select(".active-enhancedtreemap .enhancedtreemap").append("g");
 
-    var padding = 8;
+    var padding = 4;
     var fontsize = 8;
 
     // add positions to the nodes using the treemap layout
-    var treemapLayout = d3.treemap().size([400, 500]).paddingOuter(padding).paddingTop(fontsize + 1.5 * padding).paddingInner(padding)(nodes);
+    var treemapLayout = d3.treemap().size([400, 500]).paddingOuter(padding).paddingTop(fontsize + 2.5 * padding).paddingInner(padding)(nodes);
 
     // decendants instead of leaves shows all nodes, not just the leaves
     svg.selectAll("rect").data(nodes.descendants()).enter()
@@ -109,8 +200,10 @@ var d3Template = `
         .append("text")
             .attr("x", function(d) { return d.x0 + 0.5 * (d.x1 - d.x0); })
             .attr("y", function(d) { return d.y0 + 0.5 * (d.y1 - d.y0) + 0.25 * fontsize; })
-            //.attr("y", function(d) { return d.y0 + fontsize + padding; })
+            .attr("left", function(d) { return d.x0; })
+            .attr("top", function(d) { return d.y0; })
             .attr("width", function(d) { return d.x1 - d.x0 - 2 * padding; })
+            .attr("height", function(d) { return d.y1 - d.y0 - 2 * padding; })
             .attr("text-anchor", "middle")
             .attr("font-size", fontsize + "px")
             .attr("fill", "rgb(224, 224, 224)")
@@ -122,12 +215,15 @@ var d3Template = `
     svg.selectAll("label").data(nodes.descendants().filter(function(d) { return d.children; })).enter()
         .append("text")
             .attr("x", function(d) { return d.x0 + 0.5 * (d.x1 - d.x0); })
-            .attr("y", function(d) { return d.y0 + fontsize + 0.5 * padding; })
+            .attr("y", function(d) { return d.y0 + fontsize + padding; })
+            .attr("width", function(d) { return d.x1 - d.x0 - 2 * padding; })
             .attr("text-anchor", "middle")
-            .text(function(d) { return d.data.name; })
             .attr("font-size", fontsize + "px")
             //.attr("font-weight", "bold")
-            .attr("fill", "rgb(224, 224, 224)");
+            .attr("fill", "rgb(224, 224, 224)")
+            .text(function(d) { return d.data.name; })
+            .call(ellipse);
+
 `;
 
 // https://github.com/stbowers/obsidian-codeblock-labels
